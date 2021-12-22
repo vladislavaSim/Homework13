535431423842d77cb262445572dfa80d30c88633
vladislavaSim <simonova.vladislava@gmail.com>
2021-12-22 17:12:30 +0200

    setup project

---
+++ main.js
@@ -0,0 +1,67 @@
+let $container = document.querySelector('.container')
+let $box = document.querySelector('.box')
+let images = []
+function getImg(e) {
+    let $card = document.createElement('div');
+    $card.classList.add('card-img')
+    let $img = document.createElement('img');
+    images.push(e.target.value)
+    $img.src = e.target.value;
+    $card.appendChild($img)
+    $box.appendChild($card)
+    $container.innerHTML = ''
+    return images
+}
+function createInput() {
+    let $input = document.createElement('input')
+    $input.classList.add('input')
+    $input.setAttribute('type', 'text')
+    $container.appendChild($input)
+    return $input
+}
+
+
+document.querySelector('.container').addEventListener('change', function (e) {
+    e.preventDefault();
+    getImg(e)
+    if($box.childNodes.length >= 2) {
+        $box.innerHTML = ''
+        makeCarousel()
+    }
+});
+console.log($box.childNodes)
+document.querySelector('.button').addEventListener('click', function (e) {
+    e.preventDefault()
+    createInput()
+
+})
+
+
+function makeCarousel() {
+    let $buttonHolder = document.createElement('div');
+    $buttonHolder.innerHTML = '<button class="prev"> < </button> <button class="next"> > </button>'
+
+    let index = 0
+    let $img = document.createElement('img');
+    $img.src = images[index]
+    $box.append($img, $buttonHolder)
+
+    $box.addEventListener('click', function (e) {
+        if(e.target.classList.contains('prev') && index > 0){
+            --index
+            $img.src = images[index]
+            console.log(index)
+        }
+        if(e.target.classList.contains('next') && index < images.length - 1){
+            ++index
+            $img.src = images[index]
+            console.log(index)
+        }
+    })
+}
+
+//'https://i.pinimg.com/originals/d6/7c/5a/d67c5a857601f3efc0e5426a377005ba.png',
+// 'https://img-9gag-fun.9cache.com/photo/aeD4YRB_460s.jpg',
+// 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRa2UznGIoCUO-Zll0l17csnSsSQDFwsMxGrg&usqp=CAU',
+// 'https://previews.123rf.com/images/ninamunha/ninamunha1807/ninamunha180700028/104219829-summer-fruits-vector-watermelon-with-dots-cute-cartoon-characters-.jpg',
+// 'https://thumbs.gfycat.com/AdoredRichAustralianshelduck-max-1mb.gif'
